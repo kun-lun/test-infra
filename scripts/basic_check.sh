b788fd9b166339539039ba65e2435ab370366203
Andy Liu <andyliuliming@outlook.com>
2018-10-29 20:47:21 +0800

    common unit test script.

--- scripts/basic_check.sh
+++
@@ -1,7 +0,0 @@
-set -x
-go get -u golang.org/x/lint/golint
-go get -u github.com/onsi/ginkgo/ginkgo
-go get -u github.com/onsi/gomega/...
-export PATH="$PATH:$GOPATH/bin"
-golint `go list ./... | grep -v ./vendor/`
-ginkgo -r
