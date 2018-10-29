811b8a69963de30e0220b39cbe294dc5d2d9b951
Andy Liu <andyliuliming@outlook.com>
2018-10-29 21:48:31 +0800

    fix the unit test script.

--- scripts/unit_test.sh
+++ scripts/unit_test.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 set -e
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-pushd $DIR/..
-    project_name=$(echo $GIT_URL | awk -F/ '{print $NF}' | sed 's/.git//g')
-    mkdir -p $GOPATH/src/github.com/xplaceholder
-    mkdir $GOPATH/src/github.com/xplaceholder/${project_name}
-    cp -r $WORKSPACE/* $GOPATH/src/github.com/xplaceholder/${project_name}/
+project_name=$(echo $GIT_URL | awk -F/ '{print $NF}' | sed 's/.git//g')
+mkdir -p $GOPATH/src/github.com/xplaceholder
+mkdir $GOPATH/src/github.com/xplaceholder/${project_name}
+cp -r $WORKSPACE/* $GOPATH/src/github.com/xplaceholder/${project_name}/
+pushd $GOPATH/src/github.com/xplaceholder/${project_name}
     rm -rf ./src
-    pushd $GOPATH/src/github.com/xplaceholder/${project_name}
-        go get -u golang.org/x/lint/golint
-        go get -u github.com/onsi/ginkgo/ginkgo
-        go get -u github.com/onsi/gomega/...
-        export PATH="$PATH:$GOPATH/bin"
-        ginkgo -race -r
-    popd
+    go get -u golang.org/x/lint/golint
+    go get -u github.com/onsi/ginkgo/ginkgo
+    go get -u github.com/onsi/gomega/...
+    export PATH="$PATH:$GOPATH/bin"
+    ginkgo -race -r
 popd
