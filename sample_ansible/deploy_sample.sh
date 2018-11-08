049db616e8fc61eb441a0fcaaf13895a886e2de3
andyliuliming <andyliuliming@outlook.com>
2018-11-08 16:46:09 +0800

    multiple hosts in the same file.

--- sample_ansible/deploy_sample.sh
+++ sample_ansible/deploy_sample.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-pushd $DIR/..
+pushd $DIR
   echo "$( pwd )"
   export ANSIBLE_CONFIG=./ansible.cfg
   ansible-playbook -i inventories/sample sample.yml -vvv
