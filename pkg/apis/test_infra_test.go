650ae5ba6c7f945de3124fef55692825cb165948
Andy Liu <andyliuliming@outlook.com>
2018-11-09 23:16:31 +0800

    prepare for deployment cmd routines.

--- pkg/apis/test_infra_test.go
+++ pkg/apis/test_infra_test.go
@@ -8,11 +8,21 @@ import (
 )
 
 var _ = Describe("TestInfra", func() {
-	Describe("Produce", func() {
+	Describe("BuildSampleManifest", func() {
 		Context("Everything OK", func() {
 			It("should produce sample manifest correctly", func() {
 				testInfra := TestInfra{}
 				sampleManifest := testInfra.BuildSampleManifest()
+				Expect(sampleManifest).NotTo(BeNil())
+			})
+		})
+	})
+
+	Describe("PrepareForDeploymentCmd", func() {
+		Context("Everything OK", func() {
+			It("should prepare the folder correctly", func() {
+				testInfra := TestInfra{}
+				sampleManifest := testInfra.PrepareForDeploymentCmd()
 				Expect(sampleManifest).NotTo(BeNil())
 			})
 		})
---
+++ vendor/github.com/kun-lun/common/storage/store.go
@@ -0,0 +1,156 @@
+package storage
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+	"reflect"
+
+	"github.com/kun-lun/common/fileio"
+	uuid "github.com/nu7hatch/gouuid"
+)
+
+const (
+	STATE_SCHEMA = 1
+	STATE_FILE   = "kl-state.json"
+)
+
+type Store struct {
+	dir         string
+	fs          fs
+	stateSchema int
+}
+
+type fs interface {
+	fileio.FileWriter
+	fileio.Remover
+	fileio.AllRemover
+	fileio.Stater
+	fileio.AllMkdirer
+	fileio.DirReader
+}
+
+func NewStore(dir string, fs fs) Store {
+	return Store{
+		dir:         dir,
+		fs:          fs,
+		stateSchema: STATE_SCHEMA,
+	}
+}
+
+func (s Store) Set(state State) error {
+	_, err := s.fs.Stat(s.dir)
+	if err != nil {
+		return fmt.Errorf("Stat state dir: %s", err)
+	}
+
+	if reflect.DeepEqual(state, State{}) {
+		return nil
+	}
+
+	state.Version = s.stateSchema
+
+	if state.ID == "" {
+		uuid, err := uuid.NewV4()
+		if err != nil {
+			return fmt.Errorf("Create state ID: %s", err)
+		}
+		state.ID = uuid.String()
+	}
+
+	jsonData, err := json.MarshalIndent(state, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	stateFile := filepath.Join(s.dir, STATE_FILE)
+	err = s.fs.WriteFile(stateFile, jsonData, os.FileMode(0644))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s Store) GetStateDir() string {
+	return s.dir
+}
+
+func (s Store) GetVarsDir() (string, error) {
+	return s.getDir("vars", StateMode)
+}
+
+// GetQuestionaireFilePath get questionaire file path
+func (s Store) GetQuestionaireFilePath() (string, error) {
+	varsDir, err := s.GetVarsDir()
+	if err != nil {
+		return "", err
+	}
+	return path.Join(varsDir, "questionaire.yml"), nil
+}
+
+// GetArtifactsDir get artifacts folder
+func (s Store) GetArtifactsDir() (string, error) {
+	return s.getDir("artifacts", os.ModePerm)
+}
+
+// GetMainArtifactFilePath get artifacts main file path
+func (s Store) GetMainArtifactFilePath() (string, error) {
+	artifactsDir, err := s.GetArtifactsDir()
+	if err != nil {
+		return "", err
+	}
+	return path.Join(artifactsDir, "main.yml"), nil
+}
+
+// GetArtifactsPatchDir get the patches folder
+func (s Store) GetArtifactsPatchDir() (string, error) {
+	return s.getDir("artifacts/patches", os.ModePerm)
+}
+
+// GetArtifractsVarFilePath get vars file for the artifacts.
+func (s Store) GetArtifractsVarFilePath() (string, error) {
+	artifactsDir, err := s.GetVarsDir()
+	if err != nil {
+		return "", err
+	}
+	return path.Join(artifactsDir, "artifacts_vars.yml"), nil
+}
+
+// GetInfraDir get the infrastructure folder.
+func (s Store) GetInfraDir() (string, error) {
+	return s.getDir("infra", os.ModePerm)
+}
+
+// GetTerraformDir get the terraform folder, this should be the sub folder of infra.
+func (s Store) GetTerraformDir() (string, error) {
+	return s.getDir("infra/terraform", os.ModePerm)
+}
+
+func (s Store) GetDeploymentsDir() (string, error) {
+	return s.getDir("deployments", os.ModePerm)
+}
+
+func (s Store) GetAnsibleDir() (string, error) {
+	return s.getDir("deployments/ansible", os.ModePerm)
+}
+
+// TODO think about merge the vars dir with the global vars dir.
+func (s Store) GetAnsibleVarsDir() (string, error) {
+	return s.getDir("deployments/ansible/vars", os.ModePerm)
+}
+
+func (s Store) GetAnsibleInventoriesDir() (string, error) {
+	return s.getDir("deployments/ansible/inventories", os.ModePerm)
+}
+
+func (s Store) getDir(name string, perm os.FileMode) (string, error) {
+	dir := filepath.Join(s.dir, name)
+	err := s.fs.MkdirAll(dir, perm)
+	if err != nil {
+		return "", fmt.Errorf("Get %s dir: %s", name, err)
+	}
+	return dir, nil
+}
